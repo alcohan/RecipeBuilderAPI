3045c733faefa0b53fc475959f80d4a8766fe9cd
unknown <adu.cohan@gmail.com>
2023-01-20 11:41:43 -0800

    Fixed the nutrition query. NULLs in the recipe nutrition under blank categories were causing it to calculate incorrectly. Add Coalesce to fix.

--- sql/select_all_template_nutrition.sql
+++ sql/select_all_template_nutrition.sql
@@ -1,50 +1,18 @@
 SELECT 
 	[Template].Template as Template
 	,[Template].TemplateID -- just used for sorting in the order they were created	
-	,SUM([Template].Multiplier * ([Target].Price + [Template].Price)) as Price
-	,SUM([Template].Multiplier * ([Target].Calories + [Template].Calories)) as Calories
-	,SUM([Template].Multiplier * ([Target].TTLFat + [Template].TTLFat)) as TTLFat
-	,SUM([Template].Multiplier * ([Target].SatFat + [Template].SatFat)) as SatFat
-	,SUM([Template].Multiplier * ([Target].Cholesterol + [Template].Cholesterol)) as Cholesterol
-	,SUM([Template].Multiplier * ([Target].Sodium + [Template].Sodium)) as Sodium
-	,SUM([Template].Multiplier * ([Target].Carbohydrates + [Template].Carbohydrates)) as Carbohydrates
-	,SUM([Template].Multiplier * ([Target].Fiber + [Template].Fiber)) as Fiber
-	,SUM([Template].Multiplier * ([Target].Sugar + [Template].Sugar)) as Sugar
-	,SUM([Template].Multiplier * ([Target].Protein + [Template].Protein)) as Protein
-	,SUM([Template].Multiplier * ([Target].IngredientQty + [Template].IngredientQty)) as IngredientQty
+	,SUM([Template].Multiplier * (COALESCE([Target].Price,0) + [Template].Price)) as Price
+	,SUM([Template].Multiplier * (COALESCE([Target].Calories,0) + [Template].Calories)) as Calories
+	,SUM([Template].Multiplier * (COALESCE([Target].TTLFat,0) + [Template].TTLFat)) as TTLFat
+	,SUM([Template].Multiplier * (COALESCE([Target].SatFat,0) + [Template].SatFat)) as SatFat
+	,SUM([Template].Multiplier * (COALESCE([Target].Cholesterol,0) + [Template].Cholesterol)) as Cholesterol
+	,SUM([Template].Multiplier * (COALESCE([Target].Sodium,0) + [Template].Sodium)) as Sodium
+	,SUM([Template].Multiplier * (COALESCE([Target].Carbohydrates,0) + [Template].Carbohydrates)) as Carbohydrates
+	,SUM([Template].Multiplier * (COALESCE([Target].Fiber,0) + [Template].Fiber)) as Fiber
+	,SUM([Template].Multiplier * (COALESCE([Target].Sugar,0) + [Template].Sugar)) as Sugar
+	,SUM([Template].Multiplier * (COALESCE([Target].Protein,0) + [Template].Protein)) as Protein
+	,SUM([Template].Multiplier * (COALESCE([Target].IngredientQty,0) + [Template].IngredientQty)) as IngredientQty
 FROM 
-(
-	SELECT 
-		Recipes.Name
-		,Recipes.RecipeID
-		,Categories.Name as Category
-		,Categories.CategoryID
-		,COALESCE(SUM(Ingredients.PortionCost * RecipeIngredients.Quantity) ,0) as Price
-		,COALESCE(SUM(Ingredients.Calories * RecipeIngredients.Quantity),0) as Calories
-		,COALESCE(SUM(Ingredients.TTLFatGrams * RecipeIngredients.Quantity),0) as TTLFat
-		,COALESCE(SUM(Ingredients.SatFatGrams * RecipeIngredients.Quantity),0) as SatFat
-		,COALESCE(SUM(Ingredients.CholesterolMiligrams * RecipeIngredients.Quantity),0) as Cholesterol
-		,COALESCE(SUM(Ingredients.SodiumMiligrams * RecipeIngredients.Quantity),0) as Sodium
-		,COALESCE(SUM(Ingredients.CarbohydratesGrams * RecipeIngredients.Quantity),0) as Carbohydrates
-		,COALESCE(SUM(Ingredients.FiberGrams * RecipeIngredients.Quantity),0) as Fiber
-		,COALESCE(SUM(Ingredients.SugarGrams * RecipeIngredients.Quantity),0) as Sugar
-		,COALESCE(SUM(Ingredients.ProteinGrams * RecipeIngredients.Quantity),0) as Protein
-		,COALESCE(SUM(RecipeIngredients.Quantity),0) as IngredientQty
-	FROM 
-		Recipes
-		LEFT JOIN RecipeIngredients ON Recipes.RecipeID = RecipeIngredients.RecipeID
-		LEFT JOIN Ingredients ON RecipeIngredients.IngredientID = Ingredients.IngredientID
-		LEFT JOIN Categories ON Ingredients.CategoryID = Categories.CategoryID
-	WHERE 
-		Recipes.RecipeID = :placeholder
-	GROUP BY
-		Recipes.Name
-		,Recipes.RecipeID
-		,Categories.Name
-		,Categories.CategoryID
-	) as [Target]
-	
-INNER JOIN 
 	(
 	SELECT --Get all templates with nutrition broken down by category & multiplier as a column
 		a.* 
@@ -126,6 +94,40 @@ INNER JOIN
 		AND TemplateCategoryModifications.RecipeID = a.TemplateID
 	
 	) as [Template]
+
+LEFT JOIN
+
+(
+	SELECT 
+		Recipes.Name
+		,Recipes.RecipeID
+		,Categories.Name as Category
+		,Categories.CategoryID
+		,COALESCE(SUM(Ingredients.PortionCost * RecipeIngredients.Quantity) ,0) as Price
+		,COALESCE(SUM(Ingredients.Calories * RecipeIngredients.Quantity),0) as Calories
+		,COALESCE(SUM(Ingredients.TTLFatGrams * RecipeIngredients.Quantity),0) as TTLFat
+		,COALESCE(SUM(Ingredients.SatFatGrams * RecipeIngredients.Quantity),0) as SatFat
+		,COALESCE(SUM(Ingredients.CholesterolMiligrams * RecipeIngredients.Quantity),0) as Cholesterol
+		,COALESCE(SUM(Ingredients.SodiumMiligrams * RecipeIngredients.Quantity),0) as Sodium
+		,COALESCE(SUM(Ingredients.CarbohydratesGrams * RecipeIngredients.Quantity),0) as Carbohydrates
+		,COALESCE(SUM(Ingredients.FiberGrams * RecipeIngredients.Quantity),0) as Fiber
+		,COALESCE(SUM(Ingredients.SugarGrams * RecipeIngredients.Quantity),0) as Sugar
+		,COALESCE(SUM(Ingredients.ProteinGrams * RecipeIngredients.Quantity),0) as Protein
+		,COALESCE(SUM(RecipeIngredients.Quantity),0) as IngredientQty
+	FROM 
+		Recipes
+		LEFT JOIN RecipeIngredients ON Recipes.RecipeID = RecipeIngredients.RecipeID
+		LEFT JOIN Ingredients ON RecipeIngredients.IngredientID = Ingredients.IngredientID
+		LEFT JOIN Categories ON Ingredients.CategoryID = Categories.CategoryID
+	WHERE 
+		Recipes.RecipeID = :placeholder
+	GROUP BY
+		Recipes.Name
+		,Recipes.RecipeID
+		,Categories.Name
+		,Categories.CategoryID
+	) as [Target]
+
 on [Template].CategoryID = [Target].CategoryID
 GROUP BY
 	[Template].Template
