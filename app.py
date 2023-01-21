70fb045d2daeaa1f9ffc864e27ac04d616b1b1a6
adrian <adu.cohan@gmail.com>
2023-01-21 00:36:32 +0000

    Configured for SSH development on remote server

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 # Import modules
 from routes import routes
 from demo import demo
+import debugpy
+
+debugpy.listen(("0.0.0.0",5678))
 
 app = Flask(__name__)
 app.register_blueprint(routes)
