9da435ad66137e0cbeea4fa0f5717345c9b419c9
adrian <adu.cohan@gmail.com>
2023-01-26 18:40:59 +0000

    Fixed debugpy. Due to live-reload it was loading app.py twice hence causing a port issue

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from flask_cors import CORS
 # Import modules
 from routes import routes
 from demo import demo
-# import debugpy
+import debugpy
 
-# debugpy.listen(("0.0.0.0",5678))
+try: 
+    debugpy.listen(("0.0.0.0",5678))
+except RuntimeError:
+    print("Runtime error... skipping this attach")
+    pass
 
 app = Flask(__name__)
 app.register_blueprint(routes)
